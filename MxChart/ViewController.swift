7b692b078c207e8ae727a03d290f2b8cb8e161a8
Paolo Bosetti <paolo.bosetti@unitn.it>
2014-10-14 13:05:17 +0200

    Initial Commit

---
+++ MxChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MxChart
+//
+//  Created by Paolo Bosetti on 14/10/14.
+//  Copyright (c) 2014 UniTN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
